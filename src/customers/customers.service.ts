6cbfcd2b202fbba6a86d264ecd20ea87fe491780
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2022-12-13 18:49:39 +0500

    changed customer service

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -5,7 +5,6 @@ import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { Customer } from './entities/customer.entity';
 
-
 // oxiriga yetmadi
 @Injectable()
 export class CustomersService {
@@ -19,18 +18,24 @@ export class CustomersService {
     createCustomerDto = {
       hashed_password: hashpassword,
       ...createCustomerDto,
-    }
+    };
     const customer = await this.customerRapository.create(createCustomerDto);
     return { message: 'Successfully added, info: customer' };
   }
 
   async findAll() {
-    const customers = await this.customerRapository.findAll();
+    const customers = await this.customerRapository.findAll({
+      include: { all: true },
+    });
     return customers;
   }
 
   async findOne(id: number) {
-    const customer = await this.customerRapository.findOne({ where: { id } });
+    const customer = await this.customerRapository.findOne({
+      where: { id },
+      include: { all: true },
+    });
+    
     if (!customer) {
       throw new NotFoundException('No such customer exists');
     }
